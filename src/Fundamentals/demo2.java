2fb6ce3c0dcb95752b25faf6ee618ba3f1ebe395
Naveen <naveeinsingh@gmail.com>
2024-05-16 23:02:28 +0530

    16May23

---
+++ src/Fundamentals/demo2.java
@@ -0,0 +1,9 @@
+package Fundamentals;
+
+public class demo2 {
+	public static void main(String[] args) {
+		System.out.println("jo likhna hai likho!");
+		System.out.println("jaldi aao");
+		System.out.println("C");
+	}
+}
